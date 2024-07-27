e34bcc3d06dbdb7b9ac1172c144cef5361c9cfbc
bm-mit <minhngoc2891211@gmail.com>
2024-07-27 14:28:12 +0700

    fix: parser output wrong value

--- Commands/Branch/BranchOptions.cs
+++ Commands/Branch/BranchOptions.cs
@@ -5,10 +5,10 @@ namespace GitBrief.Commands.Branch;
 [Verb("branch", HelpText = "Do something with branches")]
 public class BranchOptions
 {
-    [Verb("list", isDefault: true)]
+    [Verb("list")]
     public class ListOptions
     {
-        [Value(2, HelpText = "Path to git repository, using PWD if omit.", Default = true)]
+        [Value(0, HelpText = "Path to git repository, using PWD if omit.", Required = false)]
         public string? Path { get; set; }
     }
 }
