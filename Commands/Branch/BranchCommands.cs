190ee54c83182ee812e024b25a05e54db2e3bd6e
bm-mit <minhngoc2891211@gmail.com>
2024-07-27 15:30:35 +0700

    feat: list branch and it's upstream

--- Commands/Branch/BranchCommands.cs
+++ Commands/Branch/BranchCommands.cs
@@ -6,6 +6,17 @@ public static class BranchCommands
 {
     public static void List(BranchOptions.ListOptions options)
     {
-        GitBranch.List(options.Path);
+        string? directory = options.Path;
+        var branches = GitBranch.List(directory);
+
+        foreach (var b in branches.Where(b => !b.IsRemote))
+        {
+            string prefix = GitBranch.IsCurrentBranch(b, directory) ? " * " : "   ";
+
+            Console.WriteLine(
+                b.IsTracking
+                    ? $"{prefix}{b.FriendlyName} -> {GitBranch.GetFullRemoteName(b)}"
+                    : $"{prefix}{b.FriendlyName}");
+        }
     }
 }
--- Utils/GitWrapper/GitBranch.cs
+++ Utils/GitWrapper/GitBranch.cs
@@ -1,14 +1,38 @@
+using GitBrief.Commands.Branch;
 using LibGit2Sharp;
 
 namespace GitBrief.Utils.GitWrapper;
 
 public static class GitBranch
 {
-    public static void List(string? directory)
+    public static Branch[] List(string? directory)
     {
-        Repository repo = GitRepo.Init(directory);
-        
-        foreach(Branch b in repo.Branches.Where(b => !b.IsRemote))
-            Console.WriteLine(b.FriendlyName);
+        var repo = GitRepo.Init(directory);
+        Branch[] branches = repo.Branches.ToArray();
+        Array.Sort(branches,
+            (branch1, branch2) => branch1.IsRemote == branch2.IsRemote ? 0 :
+                branch1.IsRemote ? -1 : 1);
+
+        return branches;
+    }
+
+    public static string GetFullRemoteName(Branch branch)
+    {
+        string remoteName = branch.RemoteName;
+        string remoteBranchName = branch.CanonicalName.Replace("refs/heads", remoteName);
+
+        return remoteBranchName;
+    }
+
+    public static Branch GetCurrentBranch(string? directory)
+    {
+        var repo = GitRepo.Init(directory);
+
+        return repo.Head;
+    }
+
+    public static bool IsCurrentBranch(Branch b, string? directory)
+    {
+        return b.FriendlyName == GetCurrentBranch(directory).FriendlyName;
     }
 }
